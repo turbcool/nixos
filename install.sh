927c05fc8cbba49e368af543450800e097622d91
Ilya Naydanov <turbcool@gmail.com>
2025-03-20 16:13:53 +0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+FLAKE="github:nixos-asia/website?dir=global/nixos-install-oneclick#oneclick"
+DISK_DEVICE=/dev/vda
+sudo nix \
+    --extra-experimental-features 'flakes nix-command' \
+    run github:nix-community/disko#disko-install -- \
+    --flake "$FLAKE" \
+    --write-efi-boot-entries \
+    --disk main "$DISK_DEVICE"
