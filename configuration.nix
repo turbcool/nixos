3732a69dd8f73f36c23db0b7ab4ccd63696183a3
Ilya Naidanov <turbcool@gmail.com>
2025-04-14 16:02:27 +0500

    Init

---
+++ configuration.nix
@@ -0,0 +1,215 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.supportedFilesystems = [ "ntfs" "btrfs" ];
+  fileSystems."/mnt/windows" = {
+    device = "/dev/nvme0n1p2";
+    fsType = "ntfs-3g";
+    options = [ "rw" "uid=1000" ];
+  };
+  fileSystems."/mnt/windows_old" = {
+    device = "/dev/sda4";
+    fsType = "ntfs-3g";
+    options = [ "rw" "uid=1000" ];
+  };
+  fileSystems."/mnt/hdd" = {
+    device = "/dev/sda5";
+    fsType = "btrfs";
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  
+  # L2TP
+  networking.networkmanager.enableStrongSwan = true;
+  services.xl2tpd.enable = true;
+  services.strongswan = {
+    enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Yekaterinburg";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "ru_RU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "ru";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.turb = {
+    isNormalUser = true;
+    description = "turb";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "turb";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+     remmina
+
+     # Hyprland
+     dunst
+     libnotify
+
+     #wallpaper
+     swww
+     
+     #terminal
+     kitty
+
+     rofi-wayland
+
+     brave
+     git
+     telegram-desktop
+     aider-chat
+
+     networkmanagerapplet
+     networkmanager-l2tp
+	
+     vscode
+
+     xdg-desktop-portal-gtk
+
+     yazi     
+     dolphin
+
+     lazygit
+     lazydocker
+  ];
+
+#  networking.proxy.default = "http://127.0.0.1:20172";
+
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-emoji
+      font-awesome
+    ];
+  };
+
+  services.v2raya.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  programs.waybar.enable = true;
+
+  programs.chromium = {
+    enable = true;
+    extensions = [
+	"nngceckbapebfimnlniiiahkandclblb" # bitwarden
+    ];
+    extraOpts = {
+      "PasswordManagerEnabled" = false;
+      "SyncDisabled" = true;
+      "SpellcheckEnabled" = true;
+      "SpellcheckLanguage" = [
+        "ru-RU"
+        "en-US"
+      ];
+    };
+  };
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    graphics.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
