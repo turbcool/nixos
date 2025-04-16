b553341b2f39cf7e56dc193a5061bf98f1bc6317
Ilya Naidanov <turbcool@gmail.com>
2025-04-16 23:01:15 +0500

    Nvidia driver

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -4,6 +4,13 @@
 {
   hardware = {
     graphics.enable = true;
-    nvidia.modesetting.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+    };
   };
+  services.xserver.videoDrivers = ["nvidia"];
 }
