0039640e07c2751434bbc44508788ee846edcde7
Ilya Naidanov <turbcool@gmail.com>
2025-05-17 15:30:45 +0500

    Final fix VSCode NET 8

--- hydenix/modules/hm/vscode.nix
+++ hydenix/modules/hm/vscode.nix
@@ -4,41 +4,12 @@ let
     with pkgs.dotnetCorePackages;
     combinePackages [
       sdk_8_0
-      aspnetcore_8_0
       sdk_9_0
-      aspnetcore_9_0
     ];
-
-  deps = (
-    ps:
-    with ps;
-    [
-      #zlib
-      #openssl.dev
-      #pkg-config
-      #stdenv.cc
-      #cmake
-      #mono
-      #msbuild
-    ]
-    ++ [ dotnet-full ]
-  );
 in
 {
   programs.vscode = {
     enable = true;
-    #package =
-    #  (pkgs.vscode.overrideAttrs (prevAttrs: {
-    #    nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
-    #    postFixup =
-    #      prevAttrs.postFixup
-    #      + ''
-    #        wrapProgram $out/bin/code \
-    #          --set DOTNET_ROOT "${dotnet-full}" \
-    #          --prefix PATH : "~/.dotnet/tools"
-    #      '';
-    #  })).fhsWithPackages
-    #    (ps: deps ps);
     profiles.default.extensions = with pkgs.vscode-extensions; [
       ms-dotnettools.csdevkit
       ms-dotnettools.csharp
@@ -51,7 +22,10 @@ in
     "code-flags.conf".text = ''
       --ozone-platform=wayland
       --ozone-platform-hint=auto
-      --enable-features=WaylandWindowDecorations
+      --enable-features=WaylandWindowDecorations,WaylandLinuxDrmSyncobj
     '';
   };
+  home.sessionPath = [
+    "$HOME/.dotnet/tools"
+  ];
 }
