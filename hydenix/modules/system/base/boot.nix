8b8d10ab77f4c41a9521b4e48fcda1d82600888d
Ilya Naidanov <turbcool@gmail.com>
2025-11-03 17:47:27 +0500

    Upd to non-FUSE ntfs3 driver

--- hydenix/modules/system/base/boot.nix
+++ hydenix/modules/system/base/boot.nix
@@ -9,12 +9,13 @@
   boot.supportedFilesystems = [ "ntfs" "btrfs" ];
   fileSystems."/mnt/windows" = {
     device = "/dev/nvme0n1p2";
-    fsType = "ntfs-3g";
-    options = [ "rw" "uid=1000" ];
+    fsType = "ntfs3";
+    options = [ "rw" "uid=1000" "windows_names" "noatime" "big_writes" ];
   };
+
   fileSystems."/mnt/windows_old" = {
     device = "/dev/sda4";
-    fsType = "ntfs-3g";
-    options = [ "rw" "uid=1000" ];
+    fsType = "ntfs3";
+    options = [ "rw" "uid=1000" "windows_names" "noatime" "big_writes" ];
   };
 }
