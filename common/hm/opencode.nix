1c742aab5e712bdcdfd8eeb88852167ad89b48a0
false <false>
2026-03-21 18:56:39 +0500

    Настройки OpenCode

--- common/hm/opencode.nix
+++ common/hm/opencode.nix
@@ -14,5 +14,10 @@
   home.packages = with pkgs; [
     opencode
   ];
+  home.sessionVariables = {
+    OPENAI_BASE_URL = "https://ai.flexberry.org/v1";
+    OPENAI_MODEL = "flexberry/qwen3-coder-128k:30b";
+    OPENAI_TOKEN = "sk-lLCX9gY1uDJLpUx8V5SaeQ";
+  };
 }
 
