d0562bc31a856ee0e620f9d8f37a8f3368a8c311
false <false>
2026-03-21 18:58:12 +0500

    Oh my opencode

--- common/hm/opencode.nix
+++ common/hm/opencode.nix
@@ -9,6 +9,9 @@
     ".config/opencode/opencode.json" = lib.mkForce {
       source = ./config/opencode.json;
     };
+    ".config/opencode/oh-my-opencode.json" = lib.mkForce {
+      source = ./config/oh-my-opencode.json;
+    };
   };
 
   home.packages = with pkgs; [
