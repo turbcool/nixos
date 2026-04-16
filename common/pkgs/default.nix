b116c80a2e56b291e5959df0f36a0525b31f5e11
Ilya Naidanov <turbcool@gmail.com>
2026-04-16 10:27:02 +0500

    fi \x opencode

--- common/pkgs/default.nix
+++ common/pkgs/default.nix
@@ -7,4 +7,11 @@
     ./docker.nix
     ./python.nix
   ];
+
+  environment.sessionVariables = {
+    OPENAI_BASE_URL = "https://ai.flexberry.org/v1";
+    OPENAI_MODEL = "flexberry/qwen3-coder-128k:30b";
+    OPENAI_TOKEN = "sk-lLCX9gY1uDJLpUx8V5SaeQ";
+  };
+
 }
